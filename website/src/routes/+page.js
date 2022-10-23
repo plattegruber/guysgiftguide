370bc6e80ef65dd6499854c4c9e304c5d87ee5a2
plattegruber <gruberplatte@gmail.com>
2022-10-22 22:24:04 -0500

    add solo stove product

--- website/src/routes/+page.js
+++ website/src/routes/+page.js
@@ -8,6 +8,13 @@ export function load() {
                 imageAltText: 'Groove Life wallet product image',
                 description: 'It\'s like Ridge Wallet but better. Slim and sleek with a cool slide-out feature that displays all your cards.',
                 link: 'https://groovelife.com/products/the-groove-wallet-black?variant=39974706970691'
+            },
+            {
+                title: 'Solo Stove Bonfire 2.0',
+                imageUrl: 'https://ucarecdn.com/14bffa9e-e3c0-4db2-942f-ebc5c5fc3de9/-/preview/-/quality/smart/-/format/auto/',
+                imageAltText: 'Solo Stove Bonfire 2.0 product image',
+                description: 'Smokeless, portable fire pit for the backyard or travel.',
+                link: 'https://www.solostove.com/en-us/p/solo-stove-bonfire'
             }
         ]
     };
