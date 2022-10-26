77b7237e2cfeb55dc76a525fc5f035572a638d85
plattegruber <gruberplatte@gmail.com>
2022-10-26 13:21:55 -0500

    update image sizes

--- website/src/routes/+page.js
+++ website/src/routes/+page.js
@@ -4,84 +4,84 @@ export function load() {
         products: [
             {
                 title: 'Groove Life Wallet',
-                imageUrl: 'https://ucarecdn.com/bb138619-556c-4a2b-a234-697644612faa/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/bb138619-556c-4a2b-a234-697644612faa/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Groove Life wallet product image',
                 description: 'It\'s like Ridge Wallet but better. Slim and sleek with a cool slide-out feature that displays all your cards.',
                 link: 'https://groovelife.com/products/the-groove-wallet-black?variant=39974706970691'
             },
             {
                 title: 'Solo Stove Bonfire 2.0',
-                imageUrl: 'https://ucarecdn.com/14bffa9e-e3c0-4db2-942f-ebc5c5fc3de9/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/14bffa9e-e3c0-4db2-942f-ebc5c5fc3de9/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Solo Stove Bonfire 2.0 product image',
                 description: 'Smokeless, portable fire pit for the backyard or travel.',
                 link: 'https://www.solostove.com/en-us/p/solo-stove-bonfire'
             },
             {
                 title: 'The Hero\'s Journal',
-                imageUrl: 'https://ucarecdn.com/cb9a3239-cb79-46ba-ba3e-a7884d4e4010/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/cb9a3239-cb79-46ba-ba3e-a7884d4e4010/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'The Hero\'s Journal product image',
                 description: 'Turn your own story into an epic adventure. This journal tracks goals and turns YOU into the main character.',
                 link: 'https://theherosjournal.co/collections/the-heros-journal/products/copy-of-the-heros-journal-journal-pdf'
             },
             {
                 title: 'Tile Slim',
-                imageUrl: 'https://ucarecdn.com/4e210ac1-90e5-4043-bb1a-d16fa868d8d6/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/4e210ac1-90e5-4043-bb1a-d16fa868d8d6/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Tile Slim product image',
                 description: 'A cool little item tracket that\'s designed to fit in your wallet.',
                 link: 'https://www.amazon.com/Tile-1-Pack-Bluetooth-Water-Resistant-Compatible/dp/B09B2NYJ3T?th=1'
             },
             {
                 title: 'Massdrop x Sennheiser HD 6XX Headphones',
-                imageUrl: 'https://ucarecdn.com/e1428540-c58c-40d2-8302-2a8e30affab1/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/e1428540-c58c-40d2-8302-2a8e30affab1/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Massdrop x Sennheiser HD 6XX Headphones product image',
                 description: 'The highly touted open-backed headphones recommended by audiophiles.',
                 link: 'https://drop.com/buy/massdrop-sennheiser-hd6xx'
             },
             {
                 title: 'Geekboards Soyuz desk mat',
-                imageUrl: 'https://ucarecdn.com/b9804d5c-5404-4114-a4c9-a3ea8df439e3/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/b9804d5c-5404-4114-a4c9-a3ea8df439e3/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Geekboards Soyuz desk mat product image',
                 description: 'An awesome, high-quality desk mat that sits under the keyboard and mouse, bringing character to the workspace.',
                 link: 'https://drop.com/buy/geekboards-soyuz-stitched-edge-thick-cloth-desk-mats?searchId=03af64a4fb6394a9f2bbcb2b0d3d4fe9'
             },
             {
                 title: 'Tidbyt',
-                imageUrl: 'https://ucarecdn.com/f758e4ce-cd29-4a29-8ab8-bd68d75347e5/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/f758e4ce-cd29-4a29-8ab8-bd68d75347e5/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Tidbyt product image',
                 description: 'A customizable retro display that fits in any space. Shows sports scores, crypto prices, the weather, Spotify information - basically whatever you want!',
                 link: 'https://tidbyt.com/products/tidbyt'
             },
             {
                 title: 'Darkplates 2.0',
-                imageUrl: 'https://ucarecdn.com/547f006a-93c8-4132-9ebe-6a3568aaed6d/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/547f006a-93c8-4132-9ebe-6a3568aaed6d/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Darkplates 2.0 product image',
                 description: 'Replaces the outer shell of the Playstation 5 with a custom one, including custom colors, fan vents, and leds.',
                 link: 'https://dbrand.com/shop/special-edition/ps5'
             },
             {
                 title: 'Lifelong Office Chair Wheels',
-                imageUrl: 'https://ucarecdn.com/d1e3905e-5da3-433d-b789-d3f7ab221521/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/d1e3905e-5da3-433d-b789-d3f7ab221521/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Lifelong Office Chair Wheels product image',
                 description: 'Upgrade your sitting experience with these roller blade wheels - the lifehack that improves any office chair!',
                 link: 'https://www.amazon.com/wheels-replacement-casters-hardwood-duty/dp/B01G643EU6/ref=sr_1_1_sspa?keywords=roller+blade+wheels+office+chairs&qid=1666657172&qu=eyJxc2MiOiI0LjM1IiwicXNhIjoiMy44MSIsInFzcCI6IjMuNzcifQ%3D%3D&sprefix=roller+blade+whee%2Caps%2C464&sr=8-1-spons&psc=1'
             },
             {
                 title: 'Lumin Fundamental Duo',
-                imageUrl: 'https://ucarecdn.com/84f133a8-9b2d-4517-99aa-492cbe0875ba/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/84f133a8-9b2d-4517-99aa-492cbe0875ba/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Lumin fundamental duo product image',
                 description: 'A two-step skincare routine built for guys.',
                 link: 'https://www.luminskin.com/products/fundamental-duo'
             },
             {
                 title: 'The Food Lab',
-                imageUrl: 'https://ucarecdn.com/18ec8bc6-a194-4438-9c1f-86ec27f3cfaf/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/18ec8bc6-a194-4438-9c1f-86ec27f3cfaf/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'The Food Lab product image',
                 description: 'An incredible cookbook filled science-backed techniques and suggestions from J Kenji López-Alt.',
                 link: 'https://www.target.com/p/the-food-lab-by-j-kenji-l-243-pez-alt-hardcover/-/A-79367084#lnk=sametab'
             },
             {
                 title: 'Urban Accents Movie Night Popcorn Set',
-                imageUrl: 'https://ucarecdn.com/3a1b3fff-e751-4ed2-a307-1d01322db20b/-/preview/-/quality/smart/-/format/auto/',
+                imageUrl: 'https://ucarecdn.com/3a1b3fff-e751-4ed2-a307-1d01322db20b/-/preview/-/quality/smart/-/format/auto/-/scale_crop/300x300/smart/',
                 imageAltText: 'Urban Accents Movie Night Popcorn Set product image',
                 description: 'Three kinds of popcorn with five different flavorings - this and his favorite movie would make a perfect date!',
                 link: 'https://www.amazon.com/Urban-Accents-Popcorn-Kernels-Seasoning/dp/B07JN5467R/ref=sr_1_9_mod_primary_new?crid=38NS3TAFKNP7B&keywords=men%27s+gifts&qid=1666748100&qu=eyJxc2MiOiI5LjE4IiwicXNhIjoiOC45MSIsInFzcCI6IjguNTQifQ%3D%3D&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sprefix=men%27s+gifts%2Caps%2C139&sr=8-9'
