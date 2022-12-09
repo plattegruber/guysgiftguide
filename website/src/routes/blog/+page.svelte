3c4862c6145368b3952d7cdedab2ae51af65fff4
plattegruber <plattegruber@yahoo.com>
2022-12-08 20:05:05 -0600

    add a blog post

--- website/src/routes/blog/+page.svelte
+++ website/src/routes/blog/+page.svelte
@@ -30,7 +30,7 @@
                             Jason P.
                         </p>
                         <div class="flex space-x-1 text-sm text-gray-500">
-                            <time datetime="2020-03-16">Oct 24, 2022</time>
+                            <time datetime="2020-03-16">Oct. 24th, 2022</time>
                             <span aria-hidden="true">&middot;</span>
                             <span>2 min read</span>
                         </div>
@@ -57,7 +57,34 @@
                             Jason P.
                         </p>
                         <div class="flex space-x-1 text-sm text-gray-500">
-                            <time datetime="2020-03-16">Oct 24, 2022</time>
+                            <time datetime="2020-03-16">Oct. 24th, 2022</time>
+                            <span aria-hidden="true">&middot;</span>
+                            <span>3 min read</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div class="inline-block">
+                        <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">Article</span>
+                    </div>
+                </div>
+                <a href="/blog/buying-the-perfect-gift-for-the-guy-you-just-met" class="mt-4 block">
+                    <p class="text-xl font-semibold text-gray-900">Buying the Perfect Gift for the Guy You Just Met</p>
+                    <p class="mt-3 text-base text-gray-500">Finding the perfect birthday gift for someone you don't know very well can be a daunting task. If this is your dilemma, you have...</p>
+                </a>
+                <div class="mt-6 flex items-center">
+                    <div class="flex-shrink-0">
+                        <span class="sr-only">Jason P.</span>
+                        <img class="h-10 w-10 rounded-full" src="https://ucarecdn.com/737b64d1-a06b-4474-ba37-f14d8bfdd407/-/scale_crop/100x100/smart/" alt="Jason P. profile">
+                    </div>
+                    <div class="ml-3">
+                        <p class="text-sm font-medium text-gray-900">
+                            Jason P.
+                        </p>
+                        <div class="flex space-x-1 text-sm text-gray-500">
+                            <time datetime="2020-03-16">Dec. 12th, 2022</time>
                             <span aria-hidden="true">&middot;</span>
                             <span>3 min read</span>
                         </div>
---
+++ website/src/routes/blog/buying-the-perfect-gift-for-the-guy-you-just-met/+page.svelte
@@ -0,0 +1,51 @@
+<svelte:head>
+	<title>Guys Gift Guide - Buying the Perfect Gift for the Guy You Just Met</title>
+  <meta name="description" content="Finding the perfect birthday gift for someone you don't know very well can be a daunting task. If this is your dilemma, you have...">
+</svelte:head>
+
+<div class="relative overflow-hidden bg-white py-16">
+    <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+        <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+            <svg class="absolute top-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <defs>
+                    <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    </pattern>
+                </defs>
+                <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+            </svg>
+            <svg class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <defs>
+                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    </pattern>
+                </defs>
+                <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+            </svg>
+            <svg class="absolute bottom-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                <defs>
+                    <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                    </pattern>
+                </defs>
+                <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+            </svg>
+        </div>
+    </div>
+    <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-prose text-lg">
+            <h1>
+                <span class="block text-center text-lg font-semibold text-indigo-600">Article</span>
+                <span class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Buying the Perfect Gift for the Guy You Just Met</span>
+            </h1>
+        </div>
+        <div class="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
+            <p>Finding the perfect birthday gift for someone you don't know very well can be a daunting task. If this is your dilemma, you have come to the right place! Whether you are looking for something small to let him know you care or something a bit more creative and special, there are many ways to give a thoughtful gift that won't break the bank. Let's take a look at some of the best options for celebrating his birthday. </p>
+            <p><strong>Gift Cards</strong>: If you are stumped on what to buy, an easy solution is to give him a gift card. This way he can pick out exactly what he wants or needs without feeling as if he owes you anything in return. Gift cards to Amazon, Best Buy, or even his favorite restaurant are surefire winners that will show him that you put thought into his present. </p> 
+            <p><strong>Alcoholic Beverages</strong>: Buying beer or wine is an easy way to show appreciation without getting too personal. If he loves craft beers, consider buying him a few of his favorites or introducing him to something new; if he likes wine, find out what kind of red and white wines he enjoys most. If possible, pick up two bottles—one for now and one for later—so that it looks like more than just an afterthought.  </p> 
+            <p><strong>Small Token Gifts</strong>: A great way to show someone you care is by giving them something meaningful but not overly expensive—like a book they may enjoy or tickets to their favorite event such as a movie, concert, or sports game. A unique cufflink set can also make an excellent addition if your budget allows it. No matter what type of gift you decide on, adding a personalized note with your present will help make it extra special and memorable for him.  </p> 
+            <p>No matter which type of gift you choose in the end, remember that it should reflect both who the person is and how much effort went into finding it. By taking the time to find something thoughtful and creative, your birthday present will be sure to leave an impression on anyone who receives it! So get creative and have fun shopping around until you find the perfect gift!</p>
+        </div>
+    </div>
+</div>
+  
