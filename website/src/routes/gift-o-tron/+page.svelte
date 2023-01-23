8bc14cb970c78626602018e298c718a337a8470c
plattegruber <gruberplatte@gmail.com>
2023-01-23 03:39:30 +0000

    some slight ui changes

--- website/src/routes/gift-o-tron/+page.svelte
+++ website/src/routes/gift-o-tron/+page.svelte
@@ -31,7 +31,7 @@
     </div>
     <div class="flex items-start space-x-4 mx-auto w-full max-w-xl mb-8">
         <div class="flex-shrink-0">
-            <img class="inline-block h-10 w-10 rounded-full" src="https://ucarecdn.com/e3e2c528-fb0c-45c4-9e79-2748398187c0/-/preview/50x50/" alt="">
+            <img class="inline-block h-10 w-10 rounded-full" src="https://ucarecdn.com/dafe1d70-f19a-42a0-bdc6-2c0df2e25441/-/preview/50x50/" alt="">
         </div>
         <div class="min-w-0 flex-1">
             <form method="POST" class="relative" use:enhance={({ form, data, action, cancel }) => {
@@ -41,9 +41,9 @@
                     loading = false;
                 };
               }}>
-                <div class="bg-slate-50 overflow-hidden rounded-lg border border-indigo-600 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500 p-4">
+                <div class="overflow-hidden rounded-lg border border-sky-600 shadow-sm focus-within:border-sky-600 focus-within:ring-1 focus-within:ring-indigo-500 p-4">
                     <label for="prompt" class="sr-only">Tell us about the guy you're trying to buy a gift for. The more specific the better!</label>
-                    <textarea rows="3" name="prompt" id="prompt" required class="bg-slate-50 block w-full resize-none border-0 py-3 focus:ring-0 prose sm:prose-lg" placeholder="Type your description here..."></textarea>
+                    <textarea rows="3" name="prompt" id="prompt" required class="block w-full resize-none border-0 py-3 focus:ring-0 prose sm:prose-lg" placeholder="Type your description here..."></textarea>
     
                     <!-- Spacer element to match the height of the toolbar -->
                     <div class="py-2" aria-hidden="true">
@@ -54,7 +54,7 @@
                     </div>
     
                     <div class="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2"></div>
-                    <button type="submit" class="float-right inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Generate Ideas</button>
+                    <button type="submit" class="float-right inline-flex items-center rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Generate Ideas</button>
                 </div>
             </form>
         </div>
